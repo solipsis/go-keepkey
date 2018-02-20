2d9caddb9bfe967365e180f58343e9ebad0a3506
solipsis <david@keepkey.com>
2018-02-19 22:50:08 -0700

    remove pin

--- cmd/removePin.go
+++ cmd/removePin.go
@@ -1,1 +1,24 @@
 package cmd
+
+import (
+	"log"
+
+	"github.com/spf13/cobra"
+)
+
+func init() {
+	rootCmd.AddCommand(removePinCmd)
+}
+
+var removePinCmd = &cobra.Command{
+	Use:   "removePin",
+	Short: "Disable pin on the device",
+	Long: `Disables the pin on the device. If there is currently
+		a pin then it will prompt the user to enter the current pin
+		before disabling`,
+	Run: func(cmd *cobra.Command, args []string) {
+		if err := kk.RemovePin(); err != nil {
+			log.Fatal(err)
+		}
+	},
+}
