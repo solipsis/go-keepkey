6ecd1fed177737d4707cc486d5f2b1fb8b4f14ec
solipsis <david@keepkey.com>
2018-02-23 14:50:04 -0700

    fix writeFlash

--- cmd/writeFlash.go
+++ cmd/writeFlash.go
@@ -1,31 +1,39 @@
 package cmd
 
+import (
+	"encoding/hex"
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
 func init() {
 	writeHashCmd.Flags().Uint8VarP(&sector, "sector", "s", 0, "memory sector")
-	writeHashCmd.Flags().Uint8VarP(&offset, "offset", "o", 0, "sector offset")
+	writeHashCmd.Flags().Uint16VarP(&offset, "offset", "o", 0, "sector offset")
 	writeHashCmd.Flags().StringVarP(&flashData, "data", "d", "", "data to write to flash")
 	rootCmd.AddCommand(writeHashCmd)
 }
 
 var (
-	sector uint8
-	offset uint16
+	sector    uint8
+	offset    uint16
 	flashData string
 )
 
 var writeHashCmd = &cobra.Command{
-	Use: "writeFlash",
+	Use:   "writeFlash",
 	Short: "Write data over flash sectors",
-	Long: "Writes data over flash sectors",
+	Long:  "Writes data over flash sectors",
 	Run: func(cmd *cobra.Command, args []string) {
-		
+
 		data := mustParseHex(flashData)
-		resp, err := kk.FlashHash(sector, offset, data)
+		resp, err := kk.WriteFlash(sector, offset, data)
 		if err != nil {
 			fmt.Println(err)
 			os.Exit(1)
 		}
 
 		fmt.Println(hex.EncodeToString(resp))
-	}
+	},
 }
