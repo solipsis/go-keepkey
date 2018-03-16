0bc2be618ae3dbcd48dde8747b05a915983316c5
solipsis <david@keepkey.com>
2018-03-16 10:13:55 -0600

    tests for changing settings

--- pkg/keepkey/messages_test.go
+++ pkg/keepkey/messages_test.go
@@ -33,6 +33,120 @@ func TestLoadDevice(t *testing.T) {
 	}
 }
 
+func TestApplyPolicy(t *testing.T) {
+	// enable a policy that exists
+	name := "ShapeShift"
+	if err := kk.ApplyPolicy(name, true); err != nil {
+		t.Errorf("Failed to apply policy: %s", err)
+	}
+
+	features, _ := kk.GetFeatures()
+	pass := false
+	for _, pol := range features.GetPolicies() {
+		if pol.GetPolicyName() == name && pol.GetEnabled() {
+			pass = true
+		}
+	}
+
+	if !pass {
+		t.Error("Policy not enabled")
+	}
+
+	// disable that same policy
+	if err := kk.ApplyPolicy(name, false); err != nil {
+		t.Errorf("Failed to disable policy: %s", err)
+	}
+
+	features, _ = kk.GetFeatures()
+	pass = false
+	for _, pol := range features.GetPolicies() {
+		if pol.GetPolicyName() == name && !pol.GetEnabled() {
+			pass = true
+		}
+	}
+
+	if !pass {
+		t.Error("Policy is still enabled")
+	}
+}
+
+func TestChangeLabel(t *testing.T) {
+	l1 := "TestLabel"
+	l2 := "pot@t0"
+
+	if err := kk.ApplySettings(l1, "", false); err != nil {
+		t.Error("Failed to update label:", err)
+	}
+	if feat, err := kk.GetFeatures(); err != nil || feat.GetLabel() != l1 {
+		t.Errorf("Expected label to be %s but was %s with error %s", l1, feat.GetLabel(), err)
+	}
+
+	// Change label again
+	if err := kk.ApplySettings(l2, "", false); err != nil {
+		t.Error("Failed to update label:", err)
+	}
+	if feat, err := kk.GetFeatures(); err != nil || feat.GetLabel() != l2 {
+		t.Errorf("Expected label to be %s but was %s with error %s", l2, feat.GetLabel(), err)
+	}
+}
+
+func TestEnablePassphrase(t *testing.T) {
+
+	if err := kk.ApplySettings("", "", true); err != nil {
+		t.Error("Failed to enable passphrase:", err)
+	}
+	if feat, err := kk.GetFeatures(); err != nil || !feat.GetPassphraseProtection() {
+		t.Errorf("Expected passphrase to be enabled but wasn't with error %s", err)
+	}
+
+	// Disable the passphrase
+	if err := kk.ApplySettings("", "", false); err != nil {
+		t.Error("Failed to disable passphrase:", err)
+	}
+	if feat, err := kk.GetFeatures(); err != nil || feat.GetPassphraseProtection() {
+		t.Errorf("Expected passphrase to be disabled but wasn't with error %s", err)
+	}
+
+}
+
+func TestChangeLanguage(t *testing.T) {
+	l1 := "German"
+	l2 := "english"
+
+	if err := kk.ApplySettings("", l1, false); err != nil {
+		t.Error("Failed to change language:", err)
+	}
+	if feat, err := kk.GetFeatures(); err != nil || feat.GetLanguage() != l1 {
+		t.Errorf("Expected language to be %s, but was %s, with error: %s", l1, feat.GetLanguage(), err)
+	}
+
+	// TODO; Firmware has bug where language is erased if you don't send "english" as the language
+	if err := kk.ApplySettings("", l2, false); err != nil {
+		t.Error("Failed to change language:", err)
+	}
+	if feat, err := kk.GetFeatures(); err != nil || feat.GetLanguage() != l2 {
+		t.Errorf("Expected language to be %s, but was %s, with error: %s", l2, feat.GetLanguage(), err)
+	}
+}
+
+func TestApplyInvalidPolicy(t *testing.T) {
+
+	// FIRMWARE HAS BUG AND GETS STUCK AFTER FAILING HERE
+	/*
+		// attempt to enable a policy that doesn't exist
+		name := "FakePolicy"
+			if err := kk.ApplyPolicy(name, true); err == nil {
+			t.Errorf("Expected an error but err was nil")
+		}
+	*/
+	// reset to home state after an error
+	// TODO; clean way to reset to home state after every test
+	//_, err := kk.Initialize(kk.device)
+	//if err != nil {
+	//t.Error("Unable to reset to home state", err)
+	//}
+}
+
 func TestGetFeatures(t *testing.T) {
 	features, err := kk.GetFeatures()
 	if err != nil {
@@ -81,8 +195,7 @@ func TestPing(t *testing.T) {
 
 func TestGetPublicKey(t *testing.T) {
 	// TODO: validate against known seed
-	eth := []uint32{0x8000002C, 0x8000003C, 0x80000000, 0x00000000, 0x00000000}
-	_, x, err := kk.GetPublicKey(eth, "secp256k1", false)
+	_, x, err := kk.GetPublicKey(ethPath, "secp256k1", false)
 	if err != nil || x == "" {
 		t.Fail()
 	}
